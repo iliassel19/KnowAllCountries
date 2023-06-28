43634912522bcf4b7b2dadd49eba625aed95e9f0
iliassel19 <iliasselouarti18@gmail.com>
2023-06-28 18:05:40 +0100

    Remove import of an unexesting file

--- src/pages/AllCountriesPage.jsx
+++ src/pages/AllCountriesPage.jsx
@@ -1,9 +1,8 @@
-import React, { useState, Suspense } from "react";
+import React, { Suspense } from "react";
 import { getAllCountries } from "../api/api";
 import { defer, useLoaderData, Await } from "react-router-dom";
 import Countries from "../components/Countries";
-import ShowMoreIcon from "../components/Icons/ShowMoreIcon";
-import Button from "../components/Button";
+
 import Loading from "../components/Loading";
 const AllCountries = () => {
   const { countries } = useLoaderData();
